56ccb6abad57a1455091ebad837c88eb34397935
Todd Valentic <todd.valentic@gmail.com>
2023-08-18 08:23:22 +0000

    Code cleanup in transportctl

--- src/datatransport/commands/transportctl.py
+++ src/datatransport/commands/transportctl.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+"""Transport command line interface"""
 
 ############################################################################
 #
@@ -47,10 +48,13 @@
 #               Python3: xmlrpc.client
 #               Python3: print
 #
+#   2023-08-17  Todd Valentic
+#               Improve error checking
+#               PEP8 compliance
+#
 ############################################################################
 
-from datatransport import TransportConfig
-
+import json
 import os
 import shutil
 import subprocess
@@ -58,50 +62,60 @@ import sys
 import time
 import xmlrpc.client
 
+from pathlib import Path
+
+from datatransport import TransportConfig
+
+
 class TransportControl:
+    """Transport Control client"""
 
     def __init__(self):
-
-        self.command = {    'help':     self.help,
-                            'list':     self.list,
-                            'status':   self.status,
-                            'cleanup':  self.cleanup,
-                            'reload':   self.reload,
-                            'add':      self.addGroup,
-                            'remove':   self.removeGroup,
-                            'restart':  self.restart,
-                            'start':    self.start,
-                            'stop':     self.stop       }
+        self.command = {
+            "help": self.help,
+            "list": self.list,
+            "status": self.show_status,
+            "cleanup": self.cleanup,
+            "reload": self.reload,
+            "add": self.add_group,
+            "remove": self.remove_group,
+            "restart": self.restart,
+            "start": self.start,
+            "stop": self.stop,
+            "server": self.server_control,
+        }
 
     def help(self):
+        """Show help message"""
 
         print()
-        print('-'*70)
-        print('Transport Control Commands:')
-        print('-'*70)
-        print('\thelp                     - Show this page')
-        print('\tstatus                   - Transport server status')
-        print('\tloglevel                 - Set log level on group or server')
-        print('\treload <group>           - Reload group\'s config file')
-        print('\treload server            - Reload process groups')
-        print('\tlist                     - List all process groups')
-        print('\tlist <group>             - List clients in a group')
-        print('\tstart <group>            - Start a process group')
-        print('\tstop <group>             - Stop a process group')
-        print('\trestart <group>          - Stop/start a process group')
-        print('\tadd <group>              - Notify server of new group')
-        print('\tremove <group>           - Remove a group from the server')
-        print('\tstart server             - Start the transport server')
-        print('\tstop server              - Stop the transport server')
-        print('\tstart <group> <client>   - Start the client in group')
-        print('\tstop <group> <client>    - Stop the client in group')
-        print('\tcleanup                  - Remove processes after crash')
-        print('-'*70)
+        print("-" * 70)
+        print("Transport Control Commands:")
+        print("-" * 70)
+        print("\thelp                     - Show this page")
+        print("\tstatus                   - Transport server status")
+        print("\tloglevel                 - Set log level on group or server")
+        print("\treload <group>           - Reload group's config file")
+        print("\treload server            - Reload process groups")
+        print("\tlist                     - List all process groups")
+        print("\tlist <group>             - List clients in a group")
+        print("\tstart <group>            - Start a process group")
+        print("\tstop <group>             - Stop a process group")
+        print("\trestart <group>          - Stop/start a process group")
+        print("\tadd <group>              - Notify server of new group")
+        print("\tremove <group>           - Remove a group from the server")
+        print("\tserver start             - Start the transport server")
+        print("\tserver stop              - Stop the transport server")
+        print("\tstart <group> <client>   - Start the client in group")
+        print("\tstop <group> <client>    - Stop the client in group")
+        print("\tcleanup                  - Remove processes after crash")
+        print("-" * 70)
         print()
 
-    def runCommand(self,argv):
+    def run_command(self, argv):
+        """Run command"""
 
-        if len(argv)<2:
+        if len(argv) < 2:
             self.help()
             sys.exit(1)
 
@@ -109,275 +123,334 @@ class TransportControl:
         arg = argv[2:]
 
         if cmd not in self.command:
-            print("Unknown command:",cmd)
+            print("Unknown command:", cmd)
             self.help()
             return 1
 
-        if cmd=='cleanup':
+        if cmd == "cleanup":
             self.cleanup()
             return 0
-        elif cmd=='help':
-            self.help()
-            return
 
-        url = TransportConfig().get('TransportServer','url')
+        if cmd == "help":
+            self.help()
+            return 0
+
+        url = TransportConfig().get("TransportServer", "url")
         self.server = xmlrpc.client.ServerProxy(url)
 
-        if not (cmd=='start' and len(arg)==1 and arg[0]=='server'):
+        if cmd != "server":
             # Skip if we need to start the server
 
             try:
-                self.server.status()
-            except:
-                print("Cannot connect to the transport server")
+                self.status = self.server.status()
+            except (ConnectionRefusedError, xmlrpc.client.Error) as err:
+                print(f"Cannot connect to the transport server: {err}")
                 return 1
 
         return self.command[cmd](arg)
 
-    def printProcessGroup(self,group):
-        print(group)
-        return
+    def has_group_or_exit(self, group):
+        """Exit if group does not exist"""
 
-        info = group._get_info()
+        try:
+            groups = self.server.listgroups()
+        except xmlrpc.client.Error as err:
+            print(f"Problem getting group list: {err}")
+            sys.exit(1)
 
-        print('[%s]' % info.name)
-        print('\tRunning      :',info.running)
-        print('\tLabel        :',info.label)
-        print('\tConfig files :',info.configFiles[0])
-        for name in info.configFiles[1:]:
-            print('\t             :',name)
-        print('\tLog          :',info.log)
-        print('\tStatus       :',info.status)
+        if group in groups:
+            return
 
-    def printProcessClient(self,client):
-        print(client)
-        return
+        print(f'The process group "{group}" does not exist')
+        print('  Use the "list" command to print the available groups')
 
-        print('[%s]' % client.name)
-        print('\tRunning      :',client.running)
-        print('\tLabel        :',client.label)
-        print('\tProcess ID   :',client.pid)
-        print('\tStatus       :',client.status)
+        sys.exit(1)
 
-    def status(self,arg):
-        print('Server status:',self.server.status())
+    def has_client_or_exit(self, group, client):
+        """Exit if group or client do not exist"""
+
+        self.has_group_or_exit(group)
+
+        clients = self.server.listclients(group)
+
+        if client in clients:
+            return
+
+        print(f'The client "{client}" does not exist in {group}')
+        print('  Use the "list" command to print the available clients')
+
+        sys.exit(1)
+
+    def show_status(self, _arg):
+        """Show server status"""
+
+        print("Server status:")
+        print(json.dumps(self.status, sort_keys=True, indent=4))
+
         return 0
 
-    def reload(self,arg):
+    def reload(self, arg):
+        """Reload a process group or server"""
 
-        if arg==[]:
+        if not arg:
             print('You need to specify a process group or "server"')
             return 1
 
-        if arg[0]=='server':
+        if arg[0] == "server":
             self.server.reloadgroups()
             return 0
 
-        group  = arg[0]
+        group = arg[0]
+
+        self.has_group_or_exit(group)
 
         try:
             self.server.reloadgroup(group)
-        except:
-            print('The process group "%s" does not exist' % group)
-            print('  Use the "list" command to print the available groups')
+        except xmlrpc.client.Error as err:
+            print(f"Problem reloading group: {err}")
             return 1
 
         return 0
 
-    def addGroup(self,arg):
+    def add_group(self, arg):
+        """Add a process group"""
 
-        if arg==[]:
-            print('You need to specify a process group')
+        if len(arg) == 0:
+            print("You need to specify a process group")
             return 1
+
+        name = arg[0]
 
         try:
-            self.server.addgroup(arg[0])
-        except:
-            print('There was a problem adding the group "%s"' % arg[0])
+            self.server.addgroup(name)
+        except xmlrpc.client.Error as err:
+            print(f"Problem adding the group: {err}")
             return 1
 
         return 0
 
-    def removeGroup(self,arg):
+    def remove_group(self, arg):
+        """Remove a group"""
 
-        if arg==[]:
-            print('You need to specify a process group')
+        if not arg:
+            print("You need to specify a process group")
             return 1
+
+        name = arg[0]
+
+        self.has_group_or_exit(name)
 
         try:
             self.server.removegroup(arg[0])
-        except:
-            print('There was a problem remove the group "%s"' % arg[0])
+        except xmlrpc.client.Error as err:
+            print(f"Problem removing the group: {err}")
             return 1
 
         return 0
 
-    def list(self,arg):
-        if len(arg)==1:
-            return self.listClients(arg[0])
-        else:
-            return self.listGroups()
+    def list(self, arg):
+        """List groups or the clients of a group"""
 
-    def listClients(self,group):
-        groups = self.server.listgroups()
+        if len(arg) == 1:
+            return self.list_clients(arg[0])
 
-        if group not in groups:
-            print('The process group "%s" does not exist' % group)
-            print('  Use the "list" command to print the available groups')
-            return 1
+        return self.list_groups()
+
+    def list_clients(self, group):
+        """List clients in a group"""
+
+        self.has_group_or_exit(group)
 
         clients = self.server.listclients(group)
-        print('There are',len(clients),'clients listed for the group',group)
-        for client,info in clients.items():
-            #self.printProcessClient(client)
-            print(client,info)
+        print("There are {len(clients)} clients listed for the group {group}")
+        for client, info in clients.items():
+            print(client, info)
         return 0
 
-    def listGroups(self):
+    def list_groups(self):
+        """List groups"""
+
         groups = self.server.listgroups()
+
         if not groups:
-            print('No process groups are registered')
-        else:
-            print('There are',len(groups),'groups are registered:')
-            for group,label in groups.items():
-                print('%20s - %s' % (group,label))
-                #self.printProcessGroup(group)
-        return 0
+            print("No process groups are registered")
+            return 0
 
-    def startClient(self,group,client,args):
+        width = max(len(group) for group in groups)
 
-        groups = self.server.listgroups()
-
-        if group not in groups:
-            print('The process group "%s" does not exist' % group)
-            print('  Use the "list" command to print the available groups')
-            return 1
-
-        try:
-            self.server.startclient(group,client,args)
-        except:
-            print('The client "%s" does not exist' % client)
-            return 1
+        for group, label in groups.items():
+            print(f"{group:{width}} - {label}")
 
         return 0
 
-    def stopClient(self,group,client):
+    def start_client(self, group, client, args):
+        """Start a client"""
 
-        groups = self.server.listgroups()
-
-        if group not in groups:
-            print('The process group "%s" does not exist' % group)
-            print('  Use the "list" command to print the available groups')
-            return 1
+        self.has_client_or_exit(group, client)
 
         try:
-            self.server.stopclient(group,client)
-        except:
-            print('The client "%s" does not exsit' % client)
+            self.server.startclient(group, client, args)
+        except xmlrpc.client.Error as err:
+            print(f"Problem starting client: {err}")
+            return 1
+
+        return 0
+
+    def stop_client(self, group, client):
+        """Stop a client"""
+
+        self.has_client_or_exit(group, client)
+
+        try:
+            self.server.stopclient(group, client)
+        except xmlrpc.client.Error as err:
+            print(f"Problem stopping client: {err}")
             return 1
 
         return 0
 
     def start_server(self):
+        """Start the server daemon"""
+
         args = [shutil.which("transportd"), "-b"]
+
+        # Long running process, can't use with, so disable warning
+        # pylint: disable=consider-using-with
+
         subprocess.Popen(args)
 
-    def start(self,arg):
-
-        if len(arg)==0:
-            print('You need to specify the group name or "server"')
-            return 1
-
-        if len(arg)==1 and arg[0]=='server':
-            return self.start_server()
-
-        if len(arg)>1:
-            if len(arg)==3:
-                args=arg[2]
-            else:
-                args=''
-            return self.startClient(arg[0],arg[1],args)
-
-        name = arg[0]
+    def stop_server(self):
+        """Stop the server daemon"""
 
         try:
-            self.server.startgroup(name)
-        except:
-            print('The process group "%s" does not exist' % name)
-            print('  Use the "list" command to print the available groups')
-            return 1
-
-        print('Group started:',name)
-
-        return 0
-
-    def stop(self,arg):
-
-        if len(arg)==0:
-            print('You need to provide a group name or server')
-            return 1
-
-        elif len(arg)==2:
-            self.stopClient(arg[0],arg[1])
-
-        elif arg[0]=='server':
             self.server.stop()
             time.sleep(1)
-
-        else:
-            name = arg[0]
-
-            try:
-                self.server.stopgroup(name)
-            except:
-                print('The process group "%s" does not exist' % name)
-                print('  Use the "list" command to print the available groups')
-                return 1
-
-            print('Group stopped:',name)
+        except ConnectionRefusedError:
+            pass
 
         return 0
 
-    def restart(self,arg):
+    def server_control(self, arg):
+        """Control server daemon"""
+
+        if len(arg) == 0:
+            print("Specify start, stop or restart")
+            return 1
+
+        cmd = arg[0]
+
+        if cmd == "start":
+            return self.start_server()
+
+        if cmd == "stop":
+            return self.stop_server()
+
+        if cmd == "restart":
+            self.stop_server()
+            return self.start_server()
+
+        print(f"Unknown server command: {cmd}")
+
+        return 1
+
+    def start(self, arg):
+        """Start a client or group"""
+
+        if len(arg) == 0:
+            print("You need to specify a group or client name ")
+            return 1
+
+        if len(arg) > 1:
+            if len(arg) == 3:
+                args = arg[2]
+            else:
+                args = ""
+            return self.start_client(arg[0], arg[1], args)
+
+        group = arg[0]
+
+        try:
+            self.server.startgroup(group)
+        except xmlrpc.client.Error as err:
+            print(f"Problem starting group: {err}")
+            return 1
+
+        print(f"Group started: {group}")
+
+        return 0
+
+    def stop(self, arg):
+        """Stop client, group or server"""
+
+        if not arg:
+            print("You need to provide a group or client name")
+            return 1
+
+        if len(arg) == 2:
+            group, client = arg
+            return self.stop_client(group, client)
+
+        group = arg[0]
+
+        self.has_group_or_exit(group)
+
+        try:
+            self.server.stopgroup(group)
+        except xmlrpc.client.Error as err:
+            print(f"Porblem stopping group: {err}")
+            return 1
+
+        print(f"Group stopped: {group}")
+
+        return 0
+
+    def restart(self, arg):
+        """Restart client or group"""
+
         self.stop(arg)
         self.start(arg)
 
     def cleanup(self):
+        """Stop any transport clients"""
 
         uid = TransportConfig().uid
 
-        for file in os.listdir('/proc'):
+        for path in Path("/proc").glob("*"):
+            if not path.name.isnumeric():
+                continue
+
+            statusfile = path.joinpath("status")
+
             data = {}
-            try:
-                for line in open('/proc/%s/status' % file).readlines():
-                    key,value = line.split(':')
+
+            with statusfile.open("r", encoding="utf=8") as f:
+                for line in f.readlines():
+                    key, value = line.split(":")
                     data[key] = value.strip()
-            except:
+
+            if uid != int(data["Uid"].split()[0]):
                 continue
 
-            if uid != int(data['Uid'].split()[0]):
-                continue
+            pid = int(data["Pid"])
 
-            pid = int(data['Pid'])
-
-            print('Stopping %5d - ' % pid,end=' ')
+            print(f"Stopping {pid}: ", end=" ")
             try:
-                os.kill(pid,9)
-                print('OK')
-            except:
-                print('Could not kill process')
+                os.kill(pid, 9)
+                print("OK")
+            except OSError as err:
+                print(f"Error: {err}")
 
-        print('Finished')
+        print("Finished")
 
         return 0
 
+
 def main():
+    """Script entry point"""
 
     try:
         control = TransportControl()
-        sys.exit(control.runCommand(sys.argv))
+        sys.exit(control.run_command(sys.argv))
     except KeyboardInterrupt:
         print("*** BREAK ***")
         sys.exit(1)
-
-
